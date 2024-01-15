cdd413b1212b81f617ce16fddb7e8b308301a0c5
hectoragvz <hectoragvz@gmail.com>
2024-01-15 12:47:43 -0700

    adding jwt generation for signup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import { pool } from "../db.js";
 import bcrypt from 'bcrypt';
+import { createAccessToken } from "../libs/jwt.js";
+
 
 export const signin = (req, res) => {
   res.send("Signing in...");
@@ -8,14 +10,22 @@ export const signin = (req, res) => {
 // signup
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
+
   try {
+    // hashing a password
     const hashPassword = await bcrypt.hash(password, 10);
+    //inserting a user
     const result = await pool.query(
       "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *",
       [name, email, hashPassword]
     );
-    console.log(result);
-    return res.json(result.rows[0])
+    // creating a token for the user
+    const token = await createAccessToken({id: result.rows[0].id})
+
+    return res.json({
+      token: token
+    });
+
   } catch (error) {
     if (error.code === "23505") {
       return res.status(400).json({message: "User is already registered"});
